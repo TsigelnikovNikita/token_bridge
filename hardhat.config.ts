7f678e412fdfb71c6674b1bcf44c00fa533b78c7
Nikita Tsigelnikov <nikita_tsigelnikov@mail.ru>
2022-07-19 20:08:37 +0300

    updated hardhat version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,17 +2,11 @@ import dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
 
-import "@nomiclabs/hardhat-waffle";
-import "hardhat-gas-reporter"; // https://www.npmjs.com/package/hardhat-gas-reporter
-import "@nomiclabs/hardhat-etherscan"; // https://www.npmjs.com/package/@nomiclabs/hardhat-etherscan
-import "hardhat-dependency-compiler"; // https://www.npmjs.com/package/hardhat-dependency-compiler
-import "solidity-coverage"; // https://www.npmjs.com/package/solidity-coverage
-import "hardhat-storage-layout"; // https://www.npmjs.com/package/hardhat-storage-layout
-import "@typechain/hardhat"; // https://www.npmjs.com/package/@typechain/hardhat
-import "hardhat-contract-sizer"; // https://www.npmjs.com/package/hardhat-contract-sizer
+import "@nomicfoundation/hardhat-toolbox";  // https://www.npmjs.com/package/@nomicfoundation/hardhat-toolbox
+import "hardhat-dependency-compiler";       // https://www.npmjs.com/package/hardhat-dependency-compiler
+import "hardhat-storage-layout";            // https://www.npmjs.com/package/hardhat-storage-layout
+import "hardhat-contract-sizer";            // https://www.npmjs.com/package/hardhat-contract-sizer
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (_, {ethers}) => {
   const accounts = await ethers.getSigners();
 
@@ -21,17 +15,11 @@ task("accounts", "Prints the list of accounts", async (_, {ethers}) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 dotenv.config();
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 const config: HardhatUserConfig = {
   solidity: {
-    version: process.env.COMPILE_VERSION || "0.8.4",
+    version: process.env.COMPILE_VERSION || "0.8.9",
     settings: {
       optimizer: {
         enabled: process.env.OPTIMIZER == "true",
@@ -59,9 +47,8 @@ const config: HardhatUserConfig = {
   },
   dependencyCompiler: { // add your dependencies here
     paths: [
-      "@openzeppelin/contracts/token/ERC20/IERC20.sol",
-      "@openzeppelin/contracts/token/ERC20/ERC20.sol",
-      "@openzeppelin/contracts/access/Ownable.sol"  
+      "@openzeppelin/contracts/access/Ownable.sol",
+      "@openzeppelin/contracts/token/ERC20/IERC20.sol"
     ],
   }
 };
--- test/TokenBridge.constructor.test.ts
+++ test/TokenBridge.constructor.test.ts
@@ -1,26 +1,36 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { TokenBridge, TokenBridge__factory } from "../typechain-types";
+import { TokenBridge__factory } from "../typechain-types";
 import { getAddress } from "./utils";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("TokenBridge.constructor", () => {
-  let tokenBridge : TokenBridge;
   let owner : SignerWithAddress;
+  let TokenBridge : TokenBridge__factory;
 
-  before(async () => {
-    [owner] = await ethers.getSigners();
-  });
+  async function tokenBridgeInitializeFixture() {
+    const [owner] = await ethers.getSigners();
+
+    const TokenBridge = await ethers.getContractFactory("TokenBridge");
+
+    return {TokenBridge, owner};
+  }
+
+  beforeEach(async () => {
+    ({TokenBridge, owner} = await loadFixture(tokenBridgeInitializeFixture));
+  })
 
   it("Must throw an exception if token address is zero", async () => {
-    await expect(new TokenBridge__factory(owner).deploy(ethers.constants.AddressZero))
+    await expect(TokenBridge.deploy(ethers.constants.AddressZero))
       .to.revertedWith("TokenBridge: address can't be a zero");
   });
 
   it("Must constructor contract correctly", async () => {
     const tokenAddress = await getAddress(1);
 
-    tokenBridge = await new TokenBridge__factory(owner).deploy(tokenAddress);
+    const tokenBridge = await TokenBridge.deploy(tokenAddress);
+    await tokenBridge.deployed();
 
     expect(await tokenBridge.token()).to.eq(tokenAddress);
     expect(await tokenBridge.chainId()).to.eq((await ethers.provider.getNetwork()).chainId);
